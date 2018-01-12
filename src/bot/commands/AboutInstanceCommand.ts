87394bb643415f3b8496dc8d7592506e0ff7db5d
Raphael Guntersweiler <raphael@rgunti.ch>
2018-01-12 18:58:24 +0100

    Improved About Instance command and package.json

--- src/bot/commands/AboutInstanceCommand.ts
+++ src/bot/commands/AboutInstanceCommand.ts
@@ -3,8 +3,11 @@ import 'moment-duration-format';
 import { Command, CommandoClient, CommandMessage } from 'discord.js-commando';
 import { TextChannel, RichEmbed, Client, Message } from 'discord.js';
 import { Duration, Format, Moment } from 'moment';
+import * as pjson from 'pjson';
 
 export class AboutInstanceCommand extends Command {
+    static preparedData:PreparedData;
+
     constructor(client:CommandoClient) {
         super(client, {
             name: 'aboutme',
@@ -17,9 +20,14 @@ export class AboutInstanceCommand extends Command {
                 duration: 60
             }
         });
+
+        if (!AboutInstanceCommand.preparedData) {
+            AboutInstanceCommand.preparedData = new PreparedData();
+        }
     }
 
     public run(msg:CommandMessage, args:string|object|string[], fromPattern:boolean):Promise<Message|Message[]> {
+        let data:PreparedData = AboutInstanceCommand.preparedData;
         let textChannel = msg.message.channel;
 
         let uptime:Duration = moment.duration(process.uptime(), "seconds");
@@ -29,15 +37,61 @@ export class AboutInstanceCommand extends Command {
 
         let response:RichEmbed = new RichEmbed()
             .setAuthor(this.client.user.username, this.client.user.avatarURL)
-            .setTitle(`About Discomoka 3`)
-            .setDescription(`<Discomoka3 Description here>`)
-            .addField('Version', '<Version here>', true)
+            .setTitle(`About Discomoka 3 (${data.AppName})`)
+            .setDescription(data.AppDescription)
+            .addField('Version', data.AppVersion, true)
             .addField('Platform', `Node.JS ${process.version}`, true)
             .addField('Uptime', `${uptimeStr}\n${startupStr}`, true)
             .addField('Server Time', moment().format('YYYY-MM-DD HH:mm:ss'))
-            .addField('Links', '<Links here>', false)
-            .setFooter(`© ${new Date().getFullYear()}, rGunti, All rights reserved!`);
+            .addField('Links', data.Links, false)
+            .setFooter(data.AppCopyright)
+            .setColor(0x6B8AFB);
 
         return textChannel.send({ embed: response });
+    }
+}
+
+class PreparedData {
+    links:AboutMeLink[] = [
+        new AboutMeLink(pjson.homepage, 'Homepage'),
+        new AboutMeLink(pjson.repository.url, 'Source Repository'),
+        new AboutMeLink(pjson.bugs.url, 'Bug Reporting'),
+    ];
+
+    constructor() {
+    }
+
+    public get AppName():string { return pjson.name; }
+    public get AppDescription():string { return pjson.description; }
+    public get AppVersion():string { return pjson.version; }
+    public get AppAuthor():string { return `${pjson.author.name} <${pjson.author.email}>`; }
+    public get AppRepository():string { return `${pjson.repository.url}`; }
+    public get AppLicense():string { return `${pjson.license}`; }
+
+    public get AppCopyright():string { return `© ${new Date().getFullYear()}, ${pjson.author.name}, All rights reserved!`; }
+
+    public get Links():string {
+        return this.links.map((link:AboutMeLink) => {
+            return link.URL ? link.toString() : '';
+        }).filter((value:string) => {
+            return !(!value);
+        }).join('\n');
+    }
+}
+
+class AboutMeLink {
+    url:string;
+    name:string;
+
+    constructor(url:string, name?:string) {
+        this.url = url;
+        this.name = name || url;
+    }
+
+    public get URL():string { return this.url; }
+    public get Name():string { return this.name; }
+
+    public toString():string {
+        return this.name ? `${this.name}: ${this.url}` : this.url;
     }
 }
