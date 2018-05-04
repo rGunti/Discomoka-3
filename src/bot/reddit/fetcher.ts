51c7f8fab25b499f5b6762e819ba8412f6ce7c17
Raphael Guntersweiler <raphael@rgunti.ch>
2018-05-04 22:29:35 +0200

    Fetching is implemented

--- src/bot/reddit/fetcher.ts
+++ src/bot/reddit/fetcher.ts
@@ -5,7 +5,8 @@ import * as async from 'async';
 import { RedditAutoPostSettings } from '../../db/model/Reddit';
 import { Op } from 'sequelize';
 import { CommandoClient } from 'discord.js-commando';
-import { Guild } from 'discord.js';
+import { Guild, TextChannel, RichEmbed } from 'discord.js';
+import { fetchSubreddit, RedditPost } from './utils';
 
 export class RedditFetcherStock {
     private static debugLog = debug(`discomoka3:RedditFetcherStock:Debug`);
@@ -49,6 +50,8 @@ export class RedditFetcher {
     private debugLog:debug.IDebugger;
     private errorLog:debug.IDebugger;
     private interval:number;
+    private minFetchInterval:number;
+    private doNotSave:boolean; // mostly for debugging, do not use in production
     
     private handler:NodeJS.Timer;
 
@@ -56,6 +59,8 @@ export class RedditFetcher {
         this.debugLog = debug(`discomoka3:RedditFetcher ${serverID}:Debug`);
         this.errorLog = debug(`discomoka3:RedditFetcher ${serverID}:Error`);
         this.interval = config.get('reddit.poller.interval');
+        this.minFetchInterval = config.get('reddit.serverLimits.minInterval');
+        this.doNotSave = config.has('reddit.poller.donotsave') ? config.get('reddit.poller.donotsave') : false;
         this.serverID = serverID;
 
         this.debugLog(`Setup Fetcher for Server ${serverID}`);
@@ -77,7 +82,23 @@ export class RedditFetcher {
 
             for (let r of subreddits) {
                 self.debugLog(`- ${r.subreddit} (Last updated: ${r.lastPostTimestamp}, ${r.lastPost})`);
-                
+                try {
+                    let subredditListing = await fetchSubreddit(r.subreddit);
+                    let filteredList = subredditListing.data.children.filter(i => i.data.stickied == false);
+                    if (filteredList.length > 0) {
+                        let firstItem = filteredList[0];
+                        if (firstItem.data.name != r.lastPost) {
+                            await self.sendRedditPost(firstItem.data, r);
+                        } else {
+                            await self.saveUpdatedTransaction(r);
+                        }
+                    } else {
+                        await self.saveUpdatedTransaction(r);
+                    }
+                } catch (err) {
+                    this.errorLog(`Error while fetching / processing Subreddit feed of /r/${r.subreddit}`);
+                    console.error(err);
+                }
             } 
         } catch (err) {
             this.errorLog(`Error while fetching Reddit feeds`);
@@ -99,5 +120,46 @@ export class RedditFetcher {
                 }
             }
         });
+    }
+
+    private async sendRedditPost(redditPost:RedditPost, settings:RedditAutoPostSettings, doNotSave:boolean = false) {
+        const client = RedditFetcherStock.client;
+
+        // Validation
+        const server = client.guilds.get(this.serverID);
+        if (!server) {
+            this.errorLog(`Could not post to server ${this.serverID} because the server could not be found.`);
+            return;
+        }
+
+        const channel = server.channels.get(settings.targetChannel);
+        if (!channel) {
+            this.errorLog(`Could not post to channel ${this.serverID}/${settings.targetChannel} because the channel could not be found.`);
+            return;
+        }
+
+        if (channel.type != 'text') {
+            this.errorLog(`Could not post to channel ${this.serverID}/${settings.targetChannel} because it is not a text channel (got: ${channel.type}).`);
+            return;
+        }
+
+        // Create Message
+        let message = new RichEmbed()
+            .setTitle(`New post on r/${redditPost.subreddit}`)
+            .setURL(redditPost.url || `https://reddit.com${redditPost.permalink}`)
+            .setDescription(redditPost.url);
+
+        // Send Message
+        await (<TextChannel>channel).send(message);
+
+        // Save Transaction
+        if (doNotSave) return;
+        await this.saveUpdatedTransaction(settings, redditPost.name);
+    }
+
+    private async saveUpdatedTransaction(settings:RedditAutoPostSettings, postID:string = null) {
+        settings.lastPost = postID || settings.lastPost;
+        settings.lastPostTimestamp = new Date(new Date().getTime() + settings.interval * 60000);
+        await settings.save();
     }
 }
